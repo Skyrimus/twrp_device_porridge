01152e9f7bda40750439a48b0b6ec5fbba9c1918
Skyrimus <Skyrimus@yandex.ru>
2019-04-26 13:55:30 +0300

    Update build-with-travis.sh

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,5 +1,5 @@
 mkdir -p omni
-cd /omni
+cd omni
 add-apt-repository ppa:openjdk-r/ppa 
 apt-get update 
 apt-get install bison build-essential curl ccache flex lib32ncurses5-dev lib32z1-dev libesd0-dev libncurses5-dev libsdl1.2-dev libxml2 libxml2-utils lzop pngcrush schedtool squashfs-tools xsltproc zip zlib1g-dev git-core make phablet-tools gperf openjdk-8-jdk 
