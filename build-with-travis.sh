0cba1cbc1050b556758e09601c42eb34b3068111
Skyrimus <Skyrimus@yandex.ru>
2019-04-26 12:41:43 +0300

    Update build-with-travis.sh

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -1,5 +1,7 @@
-git clone https://gitlab.com/ElXreno/android-5.1-src /build --depth=1
+mkdir build
 cd /build
+repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
+repo sync -f --force-broken --no-tags --no-clone-bundle
 mkdir -p device/wileyfox/porridge && cp -r /sources/* device/wileyfox/porridge/
 source build/envsetup.sh && lunch omni_porridge-userdebug && make -j5 recoveryimage
 cp /build/out/target/product/porridge/recovery.img /sources/recovery.img
