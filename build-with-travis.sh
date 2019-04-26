8348be47eccb6dbf24d91de7540107c35148adc1
Skyrimus <Skyrimus@yandex.ru>
2019-04-26 13:03:37 +0300

    Update build-with-travis.sh

--- build-with-travis.sh
+++ build-with-travis.sh
@@ -2,6 +2,6 @@ mkdir build
 cd /build
 repo init -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-7.1
 repo sync -f --force-broken --no-tags --no-clone-bundle
-mkdir -p device/wileyfox/porridge && cp -r /sources/* device/wileyfox/porridge/
+git clone https://github.com/Skyrimus/twrp_device_porridge.git device/wileyfox/porridge/
 source build/envsetup.sh && lunch omni_porridge-userdebug && make -j5 recoveryimage
 cp /build/out/target/product/porridge/recovery.img /sources/recovery.img
